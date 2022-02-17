66dd4ca6354440795ba45f54869ef253e383da73
Robert <64583205+Robert565@users.noreply.github.com>
2022-02-18 05:09:46 +0900

    meow

--- engine/app.py
+++ engine/app.py
@@ -10,7 +10,7 @@ def listen():  # {"query": ""}
     print('START LISTEN')
     while True:
         conn, addr = sock.accept()
-        data = conn.recv(16384*8)  # 16384
+        data = conn.recv(16384*16)  # 16384
         print(data)
         yield json.loads(data), conn
         conn.close()
